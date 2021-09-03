eb0ac43e07274f5184eea2d3e55bd9fb73d4cd0f
AbhayMaheshwari123 <43092362+AbhayMaheshwari123@users.noreply.github.com>
2021-09-03 13:08:59 +0530

    update

---
+++ script.js
@@ -0,0 +1,34 @@
+TeamName = [
+    document.querySelector('#TeamName-csk'),
+    document.querySelector('#TeamName-dc'),
+    document.querySelector('#TeamName-kxip'),
+    document.querySelector('#TeamName-kkr'),
+    document.querySelector('#TeamName-mi'),
+    document.querySelector('#TeamName-rr'),
+    document.querySelector('#TeamName-rcb'),
+    document.querySelector('#TeamName-srh'),
+
+]
+TeamVenue = [
+    document.querySelector('#venue-csk'),
+    document.querySelector('#venue-dc'),
+    document.querySelector('#venue-kxip'),
+    document.querySelector('#venue-kkr'),
+    document.querySelector('#venue-mi'),
+    document.querySelector('#venue-rr'),
+    document.querySelector('#venue-rcb'),
+    document.querySelector('#venue-srh'),
+
+]
+TeamTrophy = [
+    document.querySelector('#trophy-csk'),
+    document.querySelector('#trophy-dc'),
+    document.querySelector('#trophy-kxip'),
+    document.querySelector('#trophy-kkr'),
+    document.querySelector('#trophy-mi'),
+    document.querySelector('#trophy-rr'),
+    document.querySelector('#trophy-rcb'),
+    document.querySelector('#trophy-srh'),
+
+]
+

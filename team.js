3eba3e8384f78ec59b817fd558a1d6b529eb1a5f
AbhayMaheshwari123 <43092362+AbhayMaheshwari123@users.noreply.github.com>
2021-09-03 17:26:09 +0530

    update

---
+++ team.js
@@ -0,0 +1,30 @@
+PlayerName=[
+    document.querySelector('#dhoni'),
+    document.querySelector('#imran'),
+    document.querySelector('#ngidi'),
+    document.querySelector('#gaikwad'),
+    document.querySelector('#watson'),
+    document.querySelector('#raydu'),
+    document.querySelector('#vijay'),
+    document.querySelector('#kedar'),
+    document.querySelector('#jadeja'),
+    document.querySelector('#deepak'),
+    document.querySelector('#narayan'),
+
+]
+
+
+
+PlayerStats=[
+    document.querySelector('#narayan'),
+    document.querySelector('#narayan'),
+    document.querySelector('#narayan'),
+    document.querySelector('#narayan'),
+    document.querySelector('#narayan'),
+    document.querySelector('#narayan'),
+    document.querySelector('#narayan'),
+    document.querySelector('#narayan'),
+    document.querySelector('#narayan'),
+    document.querySelector('#narayan'),
+    document.querySelector('#narayan'),
+]

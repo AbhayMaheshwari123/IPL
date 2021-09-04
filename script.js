66f548190f4ca71ba4e4f66bbeafd1ce8677d1c3
AbhayMaheshwari123 <43092362+AbhayMaheshwari123@users.noreply.github.com>
2021-09-04 10:03:01 +0530

    changed team's html and css (scorecard div)

--- script.js
+++ script.js
@@ -32,74 +32,74 @@ TeamTrophy = [
 
 ]
 
-// const bckup=[
-//     {
-//       "id": "chennai-super-kings",
-//       "teamName": "Chennai Super Kings",
-//       "winningYears": [
-//         2010,
-//         2011,
-//         2018
-//       ],
-//       "venue": "M. A. Chidambaram Stadium"
-//     },
-//     {
-//       "id": "delhi-capitals",
-//       "teamName": "Delhi Capitals",
-//       "winningYears": [],
-//       "venue": "Feroz Shah Kotla Ground"
-//     },
-//     {
-//       "id": "kings-xi-punjab",
-//       "teamName": "Kings XI Punjab",
-//       "winningYears": [],
-//       "venue": "IS Bindra Stadium"
-//     },
-//     {
-//       "id": "kolkata-knight-riders",
-//       "teamName": "Kolkata Knight Riders",
-//       "winningYears": [
-//         2012,
-//         2014
-//       ],
-//       "venue": "Eden Gardens"
-//     },
-//     {
-//       "id": "mumbai-indians",
-//       "teamName": "Mumbai Indians",
-//       "winningYears": [
-//         2013,
-//         2015,
-//         2017,
-//         2019
-//       ],
-//       "venue": "Wankhede Stadium"
-//     },
-//     {
-//       "id": "rajasthan-royals",
-//       "teamName": "Rajasthan Royals",
-//       "winningYears": [
-//         2008
-//       ],
-//       "venue": "Sawai Mansingh Stadium"
-//     },
-//     {
-//       "id": "royal-challengers-bangalore",
-//       "teamName": "Royal Challengers Bangalore",
-//       "winningYears": [],
-//       "venue": "M. Chinnaswamy Stadium"
-//     },
-//     {
-//       "id": "sunrisers-hyderabad",
-//       "teamName": "Sunrisers Hyderabad",
-//       "winningYears": [
-//         2016
-//       ],
-//       "venue": "Rajiv Gandhi Intl. Cricket Stadium"
-//     }
-//   ]
+const bckup=[
+    {
+      "id": "chennai-super-kings",
+      "teamName": "Chennai Super Kings",
+      "winningYears": [
+        2010,
+        2011,
+        2018
+      ],
+      "venue": "M. A. Chidambaram Stadium"
+    },
+    {
+      "id": "delhi-capitals",
+      "teamName": "Delhi Capitals",
+      "winningYears": [],
+      "venue": "Feroz Shah Kotla Ground"
+    },
+    {
+      "id": "kings-xi-punjab",
+      "teamName": "Kings XI Punjab",
+      "winningYears": [],
+      "venue": "IS Bindra Stadium"
+    },
+    {
+      "id": "kolkata-knight-riders",
+      "teamName": "Kolkata Knight Riders",
+      "winningYears": [
+        2012,
+        2014
+      ],
+      "venue": "Eden Gardens"
+    },
+    {
+      "id": "mumbai-indians",
+      "teamName": "Mumbai Indians",
+      "winningYears": [
+        2013,
+        2015,
+        2017,
+        2019
+      ],
+      "venue": "Wankhede Stadium"
+    },
+    {
+      "id": "rajasthan-royals",
+      "teamName": "Rajasthan Royals",
+      "winningYears": [
+        2008
+      ],
+      "venue": "Sawai Mansingh Stadium"
+    },
+    {
+      "id": "royal-challengers-bangalore",
+      "teamName": "Royal Challengers Bangalore",
+      "winningYears": [],
+      "venue": "M. Chinnaswamy Stadium"
+    },
+    {
+      "id": "sunrisers-hyderabad",
+      "teamName": "Sunrisers Hyderabad",
+      "winningYears": [
+        2016
+      ],
+      "venue": "Rajiv Gandhi Intl. Cricket Stadium"
+    }
+  ]
 
-const fetchData = fetch("https://cors-anywhere.herokuapp.com/https://ipl-t20.herokuapp.com/teams", {
+const fetchData = fetch("https://ipl-t20.herokuapp.com/teams", {
     method: "GET",
 }).then((data) => { return data.json() }).then((finaldata) => {
     teamsdata = finaldata

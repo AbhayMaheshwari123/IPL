540088cd7bebb917e77717d3e83eb09c0032c707
AbhayMaheshwari123 <43092362+AbhayMaheshwari123@users.noreply.github.com>
2021-09-04 12:15:29 +0530

    team page responsive

--- team.js
+++ team.js
@@ -13,18 +13,45 @@ PlayerName=[
 
 ]
 
-
-
-PlayerStats=[
-    document.querySelector('#narayan'),
-    document.querySelector('#narayan'),
-    document.querySelector('#narayan'),
-    document.querySelector('#narayan'),
-    document.querySelector('#narayan'),
-    document.querySelector('#narayan'),
-    document.querySelector('#narayan'),
-    document.querySelector('#narayan'),
-    document.querySelector('#narayan'),
-    document.querySelector('#narayan'),
-    document.querySelector('#narayan'),
+Matches=[
+    document.querySelector('#match-dhoni'),
+    document.querySelector('#match-imran'),
+    document.querySelector('#match-ngidi'),
+    document.querySelector('#match-gaikwad'),
+    document.querySelector('#match-watson'),
+    document.querySelector('#match-raydu'),
+    document.querySelector('#match-vijay'),
+    document.querySelector('#match-kedar'),
+    document.querySelector('#match-jadeja'),
+    document.querySelector('#match-deepak'),
+    document.querySelector('#match-narayan'),
 ]
+
+Runs=[
+    document.querySelector('#run-dhoni'),
+    document.querySelector('#run-imran'),
+    document.querySelector('#run-ngidi'),
+    document.querySelector('#run-gaikwad'),
+    document.querySelector('#run-watson'),
+    document.querySelector('#run-raydu'),
+    document.querySelector('#run-vijay'),
+    document.querySelector('#run-kedar'),
+    document.querySelector('#run-jadeja'),
+    document.querySelector('#run-deepak'),
+    document.querySelector('#run-narayan'),
+]
+
+Wickets=[
+    document.querySelector('#wickets-dhoni'),
+    document.querySelector('#wickets-imran'),
+    document.querySelector('#wickets-ngidi'),
+    document.querySelector('#wickets-gaikwad'),
+    document.querySelector('#wickets-watson'),
+    document.querySelector('#wickets-raydu'),
+    document.querySelector('#wickets-vijay'),
+    document.querySelector('#wickets-kedar'),
+    document.querySelector('#wickets-jadeja'),
+    document.querySelector('#wickets-deepak'),
+    document.querySelector('#wickets-narayan'),
+]
+
